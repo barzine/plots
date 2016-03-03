c575ed3f25e14bd5f9ffa6d2d00f0fede78b1b60
M.P. Barzine <barzine@gmail.com>
2016-03-03 10:50:52 +0000

    Update palettes.R

--- palettes.R
+++ palettes.R
@@ -1,1 +1,8 @@
+#defined with http://tools.medialab.sciences-po.fr/iwanthue/  
+TissueCol_12<-setNames(c("#6199C1","#D1472D","#6FB241","#CA7A9F",
+                      "#C49238","#4DA28A","#7C4529","#6C68CB",
+                      "#506D33","#594C6E","#C63969","#AC8DCD"),
+                    c("Testis", "Kidney", "Adrenal", "Liver",
+                      "Esophagus", "Lung", "Prostate", "Ovary",
+                      "Heart", "Urinarybladder", "Pancreas","Colon"))
 
