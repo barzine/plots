765bc15e7a9da0ab4ca4cff79d5e6dd273e3fe0d
M.P. Barzine <barzine@gmail.com>
2016-03-07 16:55:51 +0000

    Update palettes.R
    syntax consistency

--- palettes.R
+++ palettes.R
@@ -8,7 +8,7 @@ TissueCol_12<-setNames(c("#6199C1","#D1472D","#6FB241","#CA7A9F",
                       "Esophagus", "Lung", "Prostate", "Ovary",
                       "Heart", "Urinarybladder", "Pancreas","Colon"))
 
-TissueCol_17=setNames(c("#6FB241","#AC8DCD","#C49238","#506D33",
+TissueCol_17<-setNames(c("#6FB241","#AC8DCD","#C49238","#506D33",
                         "#D1472D","#CA7A9F","#4DA28A","#6C68CB",
                         "#C63969","#7C4529","#6199C1","#D0736A",
                         "#6CB06B","#D17332","#78B626" ,"#A93F90","#CF55D8"),
