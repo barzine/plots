209e1395440798f2119bfc3ca58463d37894282b
M.P. Barzine <barzine@gmail.com>
2016-03-07 16:55:25 +0000

    TissueCol_17 added

--- palettes.R
+++ palettes.R
@@ -1,8 +1,19 @@
 #defined with http://tools.medialab.sciences-po.fr/iwanthue/  
+#try to keep the colours as consistent as possible across the sets
+
 TissueCol_12<-setNames(c("#6199C1","#D1472D","#6FB241","#CA7A9F",
                       "#C49238","#4DA28A","#7C4529","#6C68CB",
                       "#506D33","#594C6E","#C63969","#AC8DCD"),
                     c("Testis", "Kidney", "Adrenal", "Liver",
                       "Esophagus", "Lung", "Prostate", "Ovary",
                       "Heart", "Urinarybladder", "Pancreas","Colon"))
+
+TissueCol_17=setNames(c("#6FB241","#AC8DCD","#C49238","#506D33",
+                        "#D1472D","#CA7A9F","#4DA28A","#6C68CB",
+                        "#C63969","#7C4529","#6199C1","#D0736A",
+                        "#6CB06B","#D17332","#78B626" ,"#A93F90","#CF55D8"),
+                      c("Adrenal","Colon","Esophagus","Heart",
+                        "Kidney","Liver","Lung","Ovary",
+                        "Pancreas", "Prostate","Testis","Salivary",
+                        "Skin","Spleen","Stomach","Thyroid","Uterus" ))
 
