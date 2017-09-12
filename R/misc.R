2e7ae4db936519b927603f8d24f8d1b665cccef6
M.P. Barzine <barzine@gmail.com>
2017-09-12 14:43:08 +0100

    Update misc.R

--- R/misc.R
+++ R/misc.R
@@ -35,7 +35,7 @@ theme_grey<-theme_grey() %+replace% theme(plot.title=element_text(hjust=0.5))
 theme_minimaliste<-function(base_size = 11 , base_family = "", ticks = TRUE,frame=TRUE){
     ret <- theme_bw(base_family = base_family, base_size = base_size) +
         theme(panel.background = element_blank(),# panel.grid = element_blank(),
-              panel.grid.major = element_line(colour = "grey30",size=0.02),
+              panel.grid.major = element_line(colour = "gray30",size=0.02),
               panel.grid.minor = element_blank(),
               panel.border = element_blank(), #strip.background = element_blank(),
               plot.background = element_blank(),
