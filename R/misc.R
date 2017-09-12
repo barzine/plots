0b977558ff1296872f876b01adcbd89f71cd2ed0
M.P. Barzine <barzine@gmail.com>
2017-09-12 14:51:09 +0100

    Update misc.R

--- R/misc.R
+++ R/misc.R
@@ -14,7 +14,7 @@ p.list<-lapply(1:4, function(z){
                return(suppressWarnings(ggplot_gtable(ggplot_build(
                       ggplot(anscombe,
                       aes_string(x=Z1,y=Z2))+geom_point()+coord_cartesian(
-                      xlim=c(0,20),ylim=c(0,20))+theme_minimaliste(base_family=base_family)+geom_rangeframe()
+                      xlim=c(0,20),ylim=c(0,20))+theme_minimaliste(base_family=base_family)+geom_smooth()
                       ))))
                })
 ##need to be fixed later              
@@ -35,7 +35,7 @@ theme_grey<-theme_grey() %+replace% theme(plot.title=element_text(hjust=0.5))
 theme_minimaliste<-function(base_size = 11 , base_family = "", ticks = TRUE,frame=TRUE){
     ret <- theme_bw(base_family = base_family, base_size = base_size) +
         theme(panel.background = element_blank(),# panel.grid = element_blank(),
-              panel.grid.major = element_line(colour = "gray30",size=0.02),
+              panel.grid.major = element_line(colour = "gray97",size=0.02),
               panel.grid.minor = element_blank(),
               panel.border = element_blank(), #strip.background = element_blank(),
               plot.background = element_blank(),
